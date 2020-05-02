5a6fbce2800923abdbf7c636f0e88b3e67cb7d74
Korolev Pavel <korolev.vrn@gmail.com>
2020-05-02 14:45:48 +0300

    add parser

---
+++ app/controllers.py
@@ -0,0 +1,51 @@
+import requests
+from flask import render_template, request, redirect
+from app import app, db, celery
+from .models import *
+from .forms import MainForm
+from datetime import datetime
+
+
+@app.route('/')
+def index():
+    all_tasks = Tasks.query.all()
+    return render_template('index.html', all_tasks=all_tasks)
+
+
+@app.route('/results')
+def get_results():
+    results = Results.query.all()
+    return render_template('results.html', results=results)
+
+
+@app.route('/addsite', methods=['GET', 'POST'])
+def add_site():
+    form = MainForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            address = request.form.get('address')
+            task = Tasks(address=address, task_status='NOT_STARTED', timestamp=datetime.now())
+            db.session.add(task)
+            db.session.commit()
+            parser.delay(task.id)
+            return redirect('/results')
+        return render_template('add_site.html', form=form)
+
+
+@celery.task
+def parser(id):
+    task = Tasks.query.get(id)
+    task.task_status = 'PENDING'
+    db.session.commit()
+    address = task.address
+    res_address = requests.get(address)
+    count = 0
+    if res_address.status_code == 200:
+        words = res_address.text.split()
+        count = words.count("Python")
+
+    result = Results(address=address, words_count=count, http_status_code=res.status_code)
+    task = Tasks.query.get(id)
+    task.task_status = 'FINISHED'
+    db.session.add(result)
+    db.session.commit()
